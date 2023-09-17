6c998108c329d7109cc487c1dd6172f9d615cdef
de-sl <en060988@gmail.com>
2023-09-17 17:16:48 +0300

    q4 end

--- q4/Program.cs
+++ q4/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Введите перове")
+Console.WriteLine("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int mx = 0;
+int mn = 0;
+if (num1 > num2 && num1 > num3)
+    mx = num1;
+else if (num2 > num1 && num2 > num3)
+    mx = num2;
+else if (num3 > num1 && num3 > num2)
+    mx = num3;
+Console.WriteLine($"The largest number: {mx}");
+
