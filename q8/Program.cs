8a533dcd84ececdc24548df1ddfc83a572436a20
de-sl <en060988@gmail.com>
2023-09-17 17:35:55 +0300

    q8 end

---
+++ q8/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+while (res <= num){
+    Console.Write($"{res} ");
+    res++;
+}
