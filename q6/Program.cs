f69335f570e86a994915011854a388a3d008a232
de-sl <en060988@gmail.com>
2023-09-17 17:23:31 +0300

    q6 end

---
+++ q6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+    Console.WriteLine("Your number is even");
+else 
+    Console.WriteLine("Your number is not even");
